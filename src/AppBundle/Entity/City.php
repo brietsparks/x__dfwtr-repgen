ca7da7d5698128997d8d137dad10a4cc10fcf515
bsapaka <bws46@cornell.edu>
2016-12-12 21:58:45 -0600

    added city accessors

--- src/AppBundle/Entity/City.php
+++ src/AppBundle/Entity/City.php
@@ -44,4 +44,88 @@ class City
      */
     protected $subdivisions;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMls()
+    {
+        return $this->mls;
+    }
+
+    /**
+     * @param int $mls
+     * @return City
+     */
+    public function setMls($mls)
+    {
+        $this->mls = $mls;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return City
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReports()
+    {
+        return $this->reports;
+    }
+
+    /**
+     * @param mixed $reports
+     * @return City
+     */
+    public function setReports($reports)
+    {
+        $this->reports = $reports;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubdivisions()
+    {
+        return $this->subdivisions;
+    }
+
+    /**
+     * @param mixed $subdivisions
+     * @return City
+     */
+    public function setSubdivisions($subdivisions)
+    {
+        $this->subdivisions = $subdivisions;
+
+        return $this;
+    }
+
 }
